c792f9f80724c45003c88ce908e2e26a397dea4f
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-05 19:32:05 +0530

    create constants for filters

---
+++ src/lib/constants.ts
@@ -0,0 +1,120 @@
+export const LIMIT = 12;
+
+export const ROLES_OPTIONS = [
+  {
+    name: 'Frontend',
+    value: 'frontend',
+  },
+  {
+    name: 'Backend',
+    value: 'backend',
+  },
+  {
+    name: 'Fullstack',
+    value: 'fullstack',
+  },
+  {
+    name: 'iOS',
+    value: 'ios',
+  },
+  {
+    name: 'Android',
+    value: 'android',
+  },
+  {
+    name: 'Flutter',
+    value: 'flutter',
+  },
+];
+
+export const EXPERIENCE_OPTIONS = [
+  {
+    name: 'Fresher',
+    value: 0,
+  },
+  {
+    name: '1 year',
+    value: 1,
+  },
+  {
+    name: '2 years',
+    value: 2,
+  },
+  {
+    name: '3 years',
+    value: 3,
+  },
+  {
+    name: '4 years',
+    value: 4,
+  },
+  {
+    name: '5 years',
+    value: 5,
+  },
+  {
+    name: '6 years',
+    value: 6,
+  },
+  {
+    name: '7 years',
+    value: 7,
+  },
+  {
+    name: '8 years',
+    value: 8,
+  },
+  {
+    name: '9 years',
+    value: 9,
+  },
+  {
+    name: '10+ years',
+    value: 10,
+  },
+];
+export const LOCATION_OPTIONS = [
+  {
+    name: 'Remote',
+    value: 'remote',
+  },
+  {
+    name: 'On Site',
+    value: 'on-Site',
+  },
+  {
+    name: 'Hybrid',
+    value: 'hybrid',
+  },
+];
+
+export const MIN_BASE_PAY_OPTIONS = [
+  {
+    name: '3L',
+    value: 3,
+  },
+  {
+    name: '4L',
+    value: 4,
+  },
+  {
+    name: '5L',
+    value: 5,
+  },
+  {
+    name: '5L',
+    value: 5,
+  },
+  {
+    name: '6L',
+    value: 6,
+  },
+  {
+    name: '7L',
+    value: 7,
+  },
+  {
+    name: '8L',
+    value: 8,
+  },
+];
