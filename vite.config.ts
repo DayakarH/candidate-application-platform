211b24a3eb293b0e7ac163e492e9db0df7be5d43
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-05 10:15:47 +0530

    setup absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  base: './',
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@store',
+        replacement: path.resolve(__dirname, 'src/store'),
+      },
+      {
+        find: '@lib',
+        replacement: path.resolve(__dirname, 'src/lib'),
+      },
+    ],
+  },
+});
