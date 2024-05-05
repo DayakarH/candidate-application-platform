58d54c2bdb44e6bc21aba2847beb48289858e748
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-05 18:27:30 +0530

    improve design of job card

--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -1,31 +1,106 @@
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 import { forwardRef } from 'react';
 import type { JobDetails } from '../lib/types';
+import styles from './job-card.module.css';
 
 type JobCardProps = { jobDetails: JobDetails };
 
 const JobCard = forwardRef<HTMLDivElement, JobCardProps>(
   ({ jobDetails }, ref) => {
     return (
-      <Card sx={{ borderRadius: '1rem' }} ref={ref} component={"article"}>
-        <CardContent>
-          <Chip variant='outlined' label='Posted 3 days ago' />
-          <Typography>{jobDetails.companyName}</Typography>
-          <Typography variant='body1'>
-            {jobDetails.jobDetailsFromCompany}
+      <Card sx={{ borderRadius: '1rem' }} ref={ref} component={'article'}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label='logo' src={jobDetails.logoUrl}>
+              {jobDetails.companyName.charAt(0).toUpperCase()}
+            </Avatar>
+          }
+          title={
+            <Typography
+              className='title'
+              color='text.secondary'
+              className='capitalize'
+            >
+              {jobDetails.companyName}
+            </Typography>
+          }
+          subheader={
+            <>
+              <Typography
+                variant='body2'
+                color='text.secondary'
+                className='capitalize'
+              >
+                {jobDetails.jobRole}
+              </Typography>
+              <Typography
+                variant='body2'
+                color='text.secondary'
+                className='location '
+              >
+                {jobDetails.location}
+              </Typography>
+            </>
+          }
+          className={'capitalize'}
+        />
+        <CardContent sx={{ justifyContent: 'flex-start', textAlign: 'left' }}>
+          <Typography variant='body2' className={styles.salaryRange}>
+            Estimated Salary: {jobDetails?.minJdSalary ?? 0}-
+            {jobDetails?.maxJdSalary ?? 'N/A'} ✅
           </Typography>
-          <CardActions>
-            <Stack spacing={2}>
-              <Button variant='contained'>Easy Apply</Button>
-              <Button variant='contained'>Unlock Referral Asks</Button>
-            </Stack>
-          </CardActions>
+          <div className={styles.description}>
+            <Typography variant='body2'>
+              {jobDetails?.jobDetailsFromCompany}
+            </Typography>
+          </div>
+          <div className={styles.viewMore}>
+            <Button>View Job</Button>
+          </div>
+          <div style={{ marginBottom: 10 }}>
+            <Typography
+              variant='body2'
+              className={`${styles.infoContainer} ${styles.jobRequirement}`}
+            >
+              <h3>Minimum Experience:</h3>
+            </Typography>
+            <Typography variant='body2'>
+              {jobDetails?.minExp ?? '0'} years
+            </Typography>
+          </div>
+
+          <Button
+            variant='contained'
+            sx={{
+              width: '100%',
+              backgroundColor: 'rgb(85, 239, 196)',
+              color: 'rgb(0, 0, 0)',
+              fontWeight: '500',
+              padding: '8px 18px',
+            }}
+            className='capitalize'
+          >
+            ⚡ Easy Apply
+          </Button>
+          <Button
+            variant='contained'
+            sx={{
+              marginTop: '8px',
+              width: '100%',
+              backgroundColor: 'blue',
+              color: 'white',
+              fontWeight: '500',
+              padding: '8px 18px',
+            }}
+            className='capitalize'
+          >
+            Unlock Referral Asks
+          </Button>
         </CardContent>
       </Card>
     );
