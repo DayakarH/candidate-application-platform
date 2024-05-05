1122fa57fb52ee1223248a95a48c73d71c60c0cf
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-05 14:10:09 +0530

    add logic to render first 12 jobs from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import Header from '@components/layout/header';
+import { useEffect } from 'react';
+import JobCard from './components/job-card';
+import JobsContainer from './components/jobs-grid';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { fetchJobsFromAPI } from './lib/utils';
+import {
+  fetchJobsFailure,
+  fetchJobsRequest,
+  fetchJobsSuccess,
+} from './store/features/jobs';
+
 export default function App() {
+  const dispatch = useAppDispatch();
+  const { jobsList, isLoading } = useAppSelector(state => state.jobs);
+  useEffect(() => {
+    dispatch(fetchJobsRequest());
+    fetchJobsFromAPI()
+      .then(response => {
+        dispatch(fetchJobsSuccess(response));
+      })
+      .catch(error => {
+        dispatch(fetchJobsFailure(error));
+      });
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Candidate Application Platform</h1>
+      <Header />
+      <JobsContainer>
+        {!isLoading &&
+          jobsList.map(job => <JobCard jobDetails={job} key={job.jdUid} />)}
+      </JobsContainer>
     </>
   );
 }
