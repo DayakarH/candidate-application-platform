a6f8503c044cae602b1a03dff5617567ca7c81c2
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-05 16:21:52 +0530

    add grid-auto-rows declaration

--- src/components/jobs-grid.tsx
+++ src/components/jobs-grid.tsx
@@ -4,9 +4,11 @@ import type { ReactNode } from 'react';
 const JobsGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  grid-auto-rows: minmax(350px, auto);
   gap: 1rem;
+  padding-block: 2rem;
 `;
 
-export default function JobsContainer({ children }:{children: ReactNode}) {
+export default function JobsContainer({ children }: { children: ReactNode }) {
   return <JobsGrid className='container'>{children}</JobsGrid>;
 }
