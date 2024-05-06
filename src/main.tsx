165454c0e5437abda76aacff0a2180f40c70ae85
Dayakar H <dayakarheeraakri@gmail.com>
2024-05-06 15:28:54 +0530

    pass the theme to ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource-variable/lexend';
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import './index.css';
 import { store } from './store/index.ts';
+import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
